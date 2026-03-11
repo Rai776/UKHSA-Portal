9772ba097f6aad7cdfb62e09fee104c89353548d
Hewitt <c5017285@hallam.shu.ac.uk>
2026-03-11 11:48:33 +0000

    added basic css and some login

--- login.php
+++ login.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css" />
     <title>Login</title>
 </head>
 <body>
-    <div>
-        <form method="post">
+    <div class="container p-5 my-5 border">
+        <form method="post" class="form-inputs">
             <input type="email" name="email" placeholder="Email" required>
             <input type="password" name="password" placeholder="Password" required>
         
             <button type="submit" name = "login">Log in</button>
-            <button onclick="history.back()">Go Back</button>
         </form>
     </div>
 </body>
