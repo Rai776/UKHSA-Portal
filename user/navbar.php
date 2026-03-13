07ea77aef1c95d57df00e44dc24b575f8f022677
AqlanNaqib <aqlannaqib2@gmail.com>
2026-03-13 12:45:40 +0000

    navbar

---
+++ user/navbar.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <title>Dashboard</title>
+</head>
+<body>
+   
+    <?php
+    include("navbar.php");
+    ?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/style.css" />
+  <link rel="stylesheet" href="../assets/css/navbar.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+  <header>
+    <div>
+        <div>
+            <a>UKHSA</a>
+        <div>
+        <div>
+            <a href="">##Name goes here##</a>
+        </div>
+        <div>
+            <a href="dataset_catalogue.php" class= "option"><div class="material-icons">library_books</div>  Home / Catalogue</a>
+        </div>
+        <div>
+            <a href="my_requests.php" class= "option"><div class="material-icons">subject</div>  My Requests</a>
+        </div>
+        <div class="logout-item">
+            <form action="../login.php">
+                <button type="submit" name="logout" class="btn-logout">Log out</button>
+            </form>
+        </div>
+    </div>
+
+  </header>
+</body>
