24098213c8beb82be73d81e458655a4f268be709
Ben Hewitt <132229000+Rai776@users.noreply.github.com>
2026-03-13 16:11:53 +0000

    Removed dashboard.php code

--- user/navbar.php
+++ user/navbar.php
@@ -1,40 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <title>Dashboard</title>
-</head>
-<body>
-   
-    <?php
-    include("navbar.php");
-    ?>
-
-</body>
-</html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
   <title></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
